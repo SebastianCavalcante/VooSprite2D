e9353fc3a2f2e54ec1af663c6e8052aa296d41cf
Sebastian Cavalcante <sebastiancavalcante073@outlook.com>
2026-05-18 19:56:19 -0300

    Added Scripts of BonusEffects for itens

---
+++ Assets/_Project/Scripts/Coletables/ShieldEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Coletables
+{
+    public class ShieldEffect : MonoBehaviour, IColletableEffect
+    {
+        [SerializeField] private float minBonusShield;
+        [SerializeField] private float maxBonusShield;
+        
+        public void AppyEffect()
+        {
+            float bonus = Random.Range(minBonusShield, maxBonusShield);
+            GameEvents.ApllyBonus(bonus);
+        }
+    }
+}
