9dd3e45a674ef17fbdb5b205df93ce86f5ed77ce
Sebastian Cavalcante <sebastiancavalcante073@outlook.com>
2026-05-18 14:14:33 -0300

    Implemented Pooling in Class Bullet

--- Assets/_Project/Scripts/Bullets/Bullet.cs
+++ Assets/_Project/Scripts/Bullets/Bullet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Pool; // Adicionamo o namespace nativo de Pooling da unity
 
 namespace _Project.Scripts.Bullets
 {
@@ -8,6 +9,29 @@ namespace _Project.Scripts.Bullets
         public Rigidbody2D rb;
         public int totalShots;
 
+        //Guarda a referencia do pool que gerencia esta bala
+        protected IObjectPool <Bullet> objectPool;
+        
+        //Método que o shooter/Manager vai usar para configurar o pool na bala
+        public void SetPool(IObjectPool<Bullet> pool)
+        {
+            objectPool = pool;
+        }
+        
+        //Método senior: em vez de Destroy(gameObject) a bala se desativa e volta pro pool
+        protected virtual void ReleaseToPool()
+        {
+            if (objectPool != null)
+            {
+                objectPool.Release(this);
+            }
+            else
+            {
+                // FallBack caso a bala tenha sido colodaca na cena manualmente
+                Destroy(gameObject);
+            }
+        }
+        
         public virtual void OnFire()
         {
         }
