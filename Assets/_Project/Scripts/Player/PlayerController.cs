64afa044dc8f8ef4e8003d19868bd7b12f9cb076
Sebastian Cavalcante <sebastiancavalcante073@outlook.com>
2026-05-19 08:20:32 -0300

    Update PlayerController.cs
    Separate the scripts fuctions, Player Input in Update and Moviment in FixedUpdate, Removed #regions/#endregions

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -1,29 +1,27 @@
-#region Using Statements
-
 using _Project.Scripts.Managers;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-#endregion
 
 namespace _Project.Scripts.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        #region Variables
-
         private Camera _camera;
         private Rigidbody2D _rb;
         private PlayerSounds _playerSounds;
+        
+        [Header("Movement Settings")]
         [SerializeField] private float speed;
         [SerializeField] private float maxSpeed;
+        
+        [Header("Visual Effects")]
         [SerializeField] private GameObject propellant;
         [SerializeField] private GameObject propellantParticles;
 
-        #endregion
-
-        #region Initialization
-
+        // Variaveis de controle interno(dados lidos no update e aplicados no fixedupdate
+        private Vector2 _moveDirection;
+        private bool _isTrhusting;
+        
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
@@ -33,47 +31,82 @@ namespace _Project.Scripts.Player
         private void Start()
         {
             _camera = Camera.main;
+            // Impulso inicial para dar dinamismo ao jogo
             _rb.AddForce(transform.up * 1, ForceMode2D.Impulse);
         }
-
-        #endregion
-
-        #region Update Methods
-
+        
         private void Update()
         {
-            if (Mouse.current.leftButton.wasPressedThisFrame && !GameManager.Instance.gameOver)
+            if (GameManager.Instance.gameOver)
+            {
+                StopThruster();
+                return;
+            }
+
+            HandleInputs(); 
+            
+        }
+
+        private void FixedUpdate()
+        {
+            if (GameManager.Instance.gameOver)return;
+            ApplyPhysicsMovement();
+        }
+
+        private void HandleInputs()
+        {
+            // Som do propulsor ao precionar o botao
+            if (Mouse.current.leftButton.wasPressedThisFrame)
             {
                 _playerSounds.StartThrusterSound();
             }
             
-            if (Mouse.current.leftButton.isPressed && !GameManager.Instance.gameOver)
+            // Segurando o botao: Calcula direção e ativa efeitos visuais
+            if (Mouse.current.leftButton.isPressed)
             {
+                _isTrhusting = true;
                 Vector3 mousePosition = _camera.ScreenToWorldPoint(Mouse.current.position.value);
-                Vector2 direction = (mousePosition - transform.position).normalized;
-                transform.up = direction;
+                _moveDirection = (mousePosition - transform.position).normalized;
+                
+                //Rotação visual imediata e suave voltada para o mouse
+                transform.up = _moveDirection;
+                
+                propellant.SetActive(true);
+                propellantParticles.SetActive(true);
+            }
+            
+            //Soltou o botão: Cessa a aceleração da nave
+            if (Mouse.current.leftButton.wasReleasedThisFrame)
+            {
+                StopThruster();
+            }
+        }
 
-                _rb.linearVelocity = direction * speed;
-                _rb.angularVelocity = 0; // Reseta a fisica de rotação "preciso estudar mais sobre isso"
-
+        private void ApplyPhysicsMovement()
+        {
+            if (_isTrhusting)
+            {
+                // Aplica a velocidade constante na direção calculada
+                _rb.linearVelocity = _moveDirection * speed;
+                
+                // Reseta a fisica angular, fisica para travar rotações indesejadas causadas por batidas fisicas
+                // Já que controlamos a rotação atraves do transform.up via script
+                _rb.angularVelocity = 0; 
+                
+                // Limita a velocidade máxima de forma elegante
                 if (_rb.linearVelocity.magnitude > maxSpeed)
                 {
                     _rb.linearVelocity = _rb.linearVelocity.normalized * maxSpeed;
                 }
-               
-                propellant.SetActive(true);
-                propellantParticles.SetActive(true);
-            }
-
-            if (Mouse.current.leftButton.wasReleasedThisFrame)
-            {
-                _playerSounds.StopThrusterSound();
-                propellant.SetActive(false);
-                propellantParticles.SetActive(false);
-                
             }
         }
 
-        #endregion
+        private void StopThruster()
+        {
+            _isTrhusting = false;
+            _playerSounds.StopThrusterSound();
+            propellant.SetActive(false);
+            propellantParticles.SetActive(false);
+        }
     }
 }
