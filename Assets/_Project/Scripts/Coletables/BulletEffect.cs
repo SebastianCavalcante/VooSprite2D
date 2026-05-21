aa4382d74d4d99759224563b923a3906c057af1e
Sebastian Cavalcante <sebastiancavalcante073@outlook.com>
2026-05-21 10:53:57 -0300

    Create BulletEffect.cs

---
+++ Assets/_Project/Scripts/Coletables/BulletEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Coletables
+{
+    public class BulletEffect : MonoBehaviour, IColletableEffect
+    {
+        [SerializeField] private int bulletIndex;
+        
+        public void AppyEffect()
+        {
+            GameEvents.ApplyChangeWeapon(bulletIndex);
+        }
+    }
+}
